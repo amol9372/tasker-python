3eb2df26b7a07aab677e2b41bc3f32753378357d
EC2 Default User <ec2-user@ip-172-31-7-106.ap-south-1.compute.internal>
2021-05-19 08:32:32 +0000

    added region in boto

--- app/main/aws_util.py
+++ app/main/aws_util.py
@@ -1,7 +1,7 @@
 from ssm_parameter_store import EC2ParameterStore
 
 # get access id and secert from aws to run on local
-aws_param_store = EC2ParameterStore()
+aws_param_store = EC2ParameterStore(region_name="ap-south-1")
 
 google_app_credentials = aws_param_store.get_parameters_with_hierarchy(
     "/applications/google/places")
