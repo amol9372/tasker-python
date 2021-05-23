df8c4e19850918353eda9068df70862549423f07
EC2 Default User <ec2-user@ip-172-31-7-106.ap-south-1.compute.internal>
2021-05-23 06:48:16 +0000

    changed scripts

--- scripts/load_image.sh
+++ scripts/load_image.sh
@@ -1,6 +1,4 @@
 set -e
 
-cd docker.compressed.images
-aws s3 cp s3://test-bucket-acloudguru-3921/tasker-docker/tasker.tar.gz .
 unzip tasker.tar.gz
 docker load -i tasker.tar.gz
--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,5 +1,4 @@
 set -e
 
-cd docker.compressed.images
 
 docker run -it -v $HOME/.aws/credentials:/root/.aws/credentials:ro -p 5000:5000 -d tasker-backend
